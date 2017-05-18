740c44d152950c3d0780aeb7b9e19ed6e479d818
Iago Maracaja <iago079@gmail.com>
2017-05-18 13:59:22 -0300

    bug fix

--- app/src/main/java/com/mobile/pos/iago/taskmanager/views/activities/ViewTaskActivity.java
+++ app/src/main/java/com/mobile/pos/iago/taskmanager/views/activities/ViewTaskActivity.java
@@ -1,38 +1,33 @@
 package com.mobile.pos.iago.taskmanager.views.activities;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import com.mobile.pos.iago.taskmanager.R;
 import com.mobile.pos.iago.taskmanager.views.fragments.TaskListFragment;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends Activity {
+/**
+ * Created by iago on 18/05/17.
+ */
 
-    @BindView(R.id.total_of_task)
-    protected TextView mTotalOfTasks;
+public class ViewTaskActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_task);
         ButterKnife.bind(this);
         inflateFragment();
-
-        //String totalOfTask = getString(R.string.total_of_task);
-        //mTotalOfTasks.setText(totalOfTask + " " + mTasks.size());
-
     }
-
 
     /**
      * Inflate Fragment
@@ -41,21 +36,14 @@ public class MainActivity extends Activity {
         FragmentManager fm = getFragmentManager();
 
         FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.ll_fragment_list, new TaskListFragment());
+        ft.add(R.id.ll_fragment_list_view, new TaskListFragment());
         ft.commit();
 
     }
 
-
-    @OnClick(R.id.btn_close)
-    protected  void onClose(){
-        MainActivity.this.finish();
+    @OnClick(R.id.btn_create_new_task)
+    public void createNewTask() {
+        Intent createTask = new Intent(ViewTaskActivity.this, CreateTaskActivity.class);
+        startActivity(createTask);
     }
-
-    @OnClick(R.id.btn_view_task)
-    protected  void onClick(){
-        Intent it = new Intent(MainActivity.this, ViewTaskActivity.class);
-        startActivity(it);
-    }
-
 }
